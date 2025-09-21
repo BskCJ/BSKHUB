db825832dea1d552f5c2a00c9e9fd605a7697558
BskCJ <miklik1214@gmail.com>
2025-09-21 19:25:05 -0400

    main.lua

--- main.lua
+++ main.lua
@@ -1,38 +1,40 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
--- =========================
--- Window Initialization with Key System
--- =========================
 local Window = Rayfield:CreateWindow({
-    Name = "BSK UNIVERSAL HUB",
-    Icon = 0,
-    LoadingTitle = "BSK HUB",
-    LoadingSubtitle = "by bskcj",
-    ShowText = "Rayfield",
-    Theme = "Bloom",
-    ToggleUIKeybind = "K",
-    DisableRayfieldPrompts = false,
-    DisableBuildWarnings = false,
-    ConfigurationSaving = {Enabled = false},
-    Discord = {Enabled = true, Invite = "https://discord.gg/qFXqdYMgw7", RememberJoins = true},
-    KeySystem = false,
-    KeySettings = {
-        Title = "BSK HUB Key",
-        Subtitle = "Key Required",
-        Note = "Get your key from the Discord",
-        FileName = "BSKHUBKey",
-        SaveKey = true,
-        Key = "https://pastebin.com/raw/kii8F9UV",
-        Callback = function()
-            if Key == "BSKHUB2025" then
-                print("Key accepted!")
-                return true
-            else
-                warn("Invalid Key! Script will not load.")
-                return false
-            end
-        end
-    }
+   Name = "BSKHUB UNIVERSAL",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "BskHub",
+   LoadingSubtitle = "by bskcj",
+   ShowText = "BSKHUB", -- for mobile users to unhide rayfield, change if you'd like
+   Theme = "Bloom", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = false,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "BSKHUBUNIVERSALFILE"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "BskHub Key System",
+      Subtitle = "Key System",
+      Note = "Key In Discord", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"https://pastebin.com/raw/kii8F9UV"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
 })
 
 -- =========================
